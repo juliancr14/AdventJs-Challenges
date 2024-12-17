df064dede1c757c43f6bc0d746443500c6eaac7d
juliancr14 <juliancardonarojas3@gmail.com>
2024-12-17 18:51:03 -0500

    Adding html and js to complete the challenge

---
+++ AdventJS2024/FrammingNames/frammingNames.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} names - Array of names to frame
+ * @returns {string} The framed names
+ */
+function createFrame(names) {
+    
+    
+
+    return '*'
+}
+
+createFrame(['midu', 'madeval', 'educalvolpz'])
+
+// Resultado esperado:
+/* ***************
+* midu        *
+* madeval     *
+* educalvolpz *
+*************** */
+
+createFrame(['midu'])
+
+// Resultado esperado:
+/* ********
+* midu *
+******** */
+
+createFrame(['a', 'bb', 'ccc'])
+
+// Resultado esperado:
+/* *******
+* a   *
+* bb  *
+* ccc *
+******* */
+
+createFrame(['a', 'bb', 'ccc', 'dddd'])
