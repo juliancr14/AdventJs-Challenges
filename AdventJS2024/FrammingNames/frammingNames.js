d35b06f990afbfd4b958f5965e9f9b04af24533e
juliancr14 <juliancardonarojas3@gmail.com>
2025-01-10 14:53:55 -0500

    Adding solved exercise framming names

--- AdventJS2024/FrammingNames/frammingNames.js
+++ AdventJS2024/FrammingNames/frammingNames.js
@@ -4,15 +4,50 @@
  */
 function createFrame(names) {
     
-    
+    let biggestString = 0
 
-    return '*'
+    for (let i = 0; i < names.length; i++) {
+
+        let letterCounting = 0
+
+        for (const item of names[i]) {
+            letterCounting++
+        }
+
+        if (letterCounting > biggestString) {
+            biggestString = letterCounting
+        }
+    }
+
+    let frammeWidth = biggestString + 3
+    let frammework = ""
+    let frammeContent = ""
+
+    for (let i = 0; i <= frammeWidth; i++) {
+        frammework += "*"
+    }
+
+    for(let i = 0; i < names.length; i++) {
+        let frammeBetween = frammeWidth - (names[i].length + 2)
+
+        frammeContent += `* ${names[i]}`
+
+        for (let j = 0; j < frammeBetween; j++) {
+            frammeContent += " "
+        }
+
+        frammeContent += "*\n"
+    }
+
+    let framme = frammework + "\n" + frammeContent + frammework
+    
+    return framme
 }
 
 createFrame(['midu', 'madeval', 'educalvolpz'])
 
-// Resultado esperado:
-/* ***************
+/* Resultado esperado:
+***************
 * midu        *
 * madeval     *
 * educalvolpz *
@@ -20,18 +55,26 @@ createFrame(['midu', 'madeval', 'educalvolpz'])
 
 createFrame(['midu'])
 
-// Resultado esperado:
-/* ********
+/* Resultado esperado:
+********
 * midu *
 ******** */
 
 createFrame(['a', 'bb', 'ccc'])
 
-// Resultado esperado:
-/* *******
+/* Resultado esperado:
+*******
 * a   *
 * bb  *
 * ccc *
 ******* */
 
 createFrame(['a', 'bb', 'ccc', 'dddd'])
+
+/* Resultado esperado:
+********
+* a    *
+* bb   *
+* ccc  *
+* dddd *
+******** */
